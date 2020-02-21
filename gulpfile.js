06dc9d96eb8c3836ade42b70491744412035d60f
LevanovA <46813895+LevanovA@users.noreply.github.com>
2020-02-21 12:23:50 +0300

    Добавляет блок fitness на главную страницу

--- gulpfile.js
+++ gulpfile.js
@@ -87,7 +87,8 @@ gulp.task("copy", function () {
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
     "source/js/**",
-    "source//*.ico"
+    "source//*.ico",
+    "source/slick/**"
   ], {
     base: "source"
   })
