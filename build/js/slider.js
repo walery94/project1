6ef044a9fa4cc807787e4017a3bf51a06c104999
walery94 <50008337+walery94@users.noreply.github.com>
2020-03-19 17:46:11 +0300

    Исправляет слайдер

--- build/js/slider.js
+++ build/js/slider.js
@@ -1,28 +1,27 @@
 'use strict'
 
 $(document).ready(function(){
-
+  $('.slider__list').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    nextArrow: document.querySelector('.slider__button--next'),
+    prevArrow: document.querySelector('.slider__button--prev'),
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
   $(window).on('load resize', function() {
     if ($(window).width() < 1024) {
-      $('.slider__list:not(.slick-initialized)').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        nextArrow: document.querySelector('.slider__button--next'),
-        prevArrow: document.querySelector('.slider__button--prev'),
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        variableWidth: true,
-        responsive: [
-          {
-            breakpoint: 767,
-            settings: {
-              arrows: false,
-              slidesToShow: 1
-            }
-          }
-        ]
-      });
       $('.events__items:not(.slick-initialized)').slick({
         dots: false,
         infinite: true,
@@ -57,7 +56,6 @@ $(document).ready(function(){
         variableWidth: true
       });
     } else {
-      $('.slider__list.slick-initialized').slick("unslick");
       $(".events__items.slick-initialized").slick("unslick");
       $(".store__items--shops.slick-initialized").slick("unslick");
       $(".store__items--services.slick-initialized").slick("unslick");
