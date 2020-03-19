3453b880040413ff680192be58a7aa49b754a2e8
walery94 <50008337+walery94@users.noreply.github.com>
2020-03-19 10:25:47 +0300

    Добавляет фиксированное меню

--- build/js/scroll.js
+++ build/js/scroll.js
@@ -3,8 +3,10 @@
 $(document).ready(function(){
 	var pagination = $('.pagination');
 	var inputSearch = $('#search');
+	var paginationSearch = $('.pagination__search');
+	var paginationScrollIcons = $('.pagination__scroll-icons');
 	
-	var checkYOffset = function() {
+	var checkYOffsetMobile = function() {
 		if (window.pageYOffset < 56) {
 			pagination.removeClass('pagination--scroll');
 			inputSearch.attr('placeholder', 'Поиск всего');
@@ -13,7 +15,24 @@ $(document).ready(function(){
 			inputSearch.attr('placeholder', 'Поиск');
 		}
 	};
+	if (pagination && inputSearch) {
+		$(document).scroll(checkYOffsetMobile);
+		checkYOffsetMobile();
+	}
 	
-	$(document).scroll(checkYOffset);
-	checkYOffset();
+	var checkYOffsetDesktop = function () {
+		console.log(window.pageYOffset);
+		if (window.pageYOffset < 56) {
+			paginationSearch.removeClass('visually-hidden');
+			paginationScrollIcons.addClass('visually-hidden');
+		} else {
+			paginationSearch.addClass('visually-hidden');
+			paginationScrollIcons.removeClass('visually-hidden');
+		}
+	};
+	
+	if (paginationSearch && paginationScrollIcons) {
+		$(document).scroll(checkYOffsetDesktop);
+		checkYOffsetDesktop();
+	}
 });
