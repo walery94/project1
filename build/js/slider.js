57aecdf14846bd4b998791ffa44073273a2d36d9
walery94 <50008337+walery94@users.noreply.github.com>
2020-03-20 10:30:42 +0300

    Исправляет баги

--- build/js/slider.js
+++ build/js/slider.js
@@ -22,6 +22,25 @@ $(document).ready(function(){
   });
   $(window).on('load resize', function() {
     if ($(window).width() < 1024) {
+      $('.date__slider:not(.slick-initialized)').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        nextArrow: document.querySelector('.slider__button--next'),
+        prevArrow: document.querySelector('.slider__button--prev'),
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        variableWidth: true,
+        responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              arrows: false,
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
       $('.events__items:not(.slick-initialized)').slick({
         dots: false,
         infinite: true,
@@ -56,6 +75,7 @@ $(document).ready(function(){
         variableWidth: true
       });
     } else {
+      $('.date__slider.slick-initialized').slick("unslick");
       $(".events__items.slick-initialized").slick("unslick");
       $(".store__items--shops.slick-initialized").slick("unslick");
       $(".store__items--services.slick-initialized").slick("unslick");
