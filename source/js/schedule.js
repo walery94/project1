bf66b56ef2778855a7cd13260b0f8c4d81e8f6b8
walery94 <50008337+walery94@users.noreply.github.com>
2020-03-31 13:34:06 +0300

    Правит баги

--- source/js/schedule.js
+++ source/js/schedule.js
@@ -2,46 +2,57 @@
 
 $(document).ready(function(){
 	
+	var body = $('body');
 	var buttonClock = $('.site-navigation__item--clock');
 	var buttonClockFooter = $('.address__item--time');
 	var buttonClockShop = $('.page-shop__link--clock');
 	var buttonClockCinema = $('.shop-info__link--clock');
 	var buttonClose = $('.schedule__button');
 	
+	var ESC = 27;
+	
 	if (buttonClock) {
 		buttonClock.on('click', function (evt) {
 			evt.preventDefault();
-			$('body').addClass('schedule-opened');
+			body.addClass('schedule-opened');
 		});
 	}
 	
 	if (buttonClockFooter) {
 		buttonClockFooter.on('click', function (evt) {
 			evt.preventDefault();
-			$('body').addClass('schedule-opened');
+			body.addClass('schedule-opened');
 		});
 	}
 	
 	if (buttonClockShop) {
 		buttonClockShop.on('click', function (evt) {
 			evt.preventDefault();
-			$('body').addClass('schedule-opened');
+			body.addClass('schedule-opened');
 		});
 	}
 	
 	if (buttonClockCinema) {
 		buttonClockCinema.on('click', function (evt) {
 			evt.preventDefault();
-			console.log('hello');
-			$('body').addClass('schedule-opened');
+			body.addClass('schedule-opened');
 		});
 	}
 	
 	if (buttonClose) {
 		buttonClose.on('click', function (evt) {
 			evt.preventDefault();
-			$('body').removeClass('schedule-opened');
+			body.removeClass('schedule-opened');
 		});
 	}
+	
+	window.addEventListener('keydown', function (evt) {
+		if (evt.keyCode === ESC) {
+			evt.preventDefault();
+			if (body.hasClass('schedule-opened')) {
+				body.removeClass('schedule-opened');
+			}
+		}
+	});
 	
 });
