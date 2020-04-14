8704f68b8bec9d7c554cea7cd947b9009b59a402
walery94 <50008337+walery94@users.noreply.github.com>
2020-04-14 16:06:39 +0300

    Правит ошибки линтера

--- build/js/imask.js
+++ build/js/imask.js
@@ -1,20 +1,19 @@
 'use strict';
 
-$(document).ready(function(){
-	
-	var phoneAdverters = document.getElementById('phone-adverters');
-	var phoneFeedback = document.getElementById('phone-feedback');
-	
-	var optionMask = {
-		mask: '+{7} (000) 000-00-00'
-	};
-	if (phoneAdverters) {
-		var maskPhoneAdverters = IMask(phoneAdverters, optionMask);
-		maskPhoneAdverters.updateValue();
-	}
-	if (phoneFeedback) {
-		var maskPhoneFeedback = IMask(phoneFeedback, optionMask);
-		maskPhoneFeedback.updateValue();
-	}
-	
+$(document).ready(function () {
+
+  var phoneAdverters = document.getElementById('phone-adverters');
+  var phoneFeedback = document.getElementById('phone-feedback');
+
+  var optionMask = {
+    mask: '+{7} (000) 000-00-00',
+  };
+  if (phoneAdverters) {
+    var maskPhoneAdverters = IMask(phoneAdverters, optionMask);
+    maskPhoneAdverters.updateValue();
+  }
+  if (phoneFeedback) {
+    var maskPhoneFeedback = IMask(phoneFeedback, optionMask);
+    maskPhoneFeedback.updateValue();
+  }
 });
--- build/js/schedule.js
+++ build/js/schedule.js
@@ -1,67 +1,57 @@
 'use strict';
 
-$(document).ready(function(){
-	
-	// График работы
-	
-	var body = $('body');
-	var buttonClock = $('.site-navigation__item--clock');
-	var buttonClockFooter = $('.address__item--time');
-	var buttonClockShop = $('.page-shop__link--clock');
-	var buttonClockCinema = $('.shop-info__link--clock');
-	var buttonClockMenu = $('.address-menu__item--clock');
-	var buttonClose = $('.schedule__button');
-	
-	var ESC = 27;
-	
-	if (buttonClock) {
-		buttonClock.on('click', function (evt) {
-			evt.preventDefault();
-			body.addClass('schedule-opened');
-		});
-	}
-
-	if (buttonClockFooter) {
-		buttonClockFooter.on('click', function (evt) {
-			evt.preventDefault();
-			body.addClass('schedule-opened');
-		});
-	}
-
-	if (buttonClockShop) {
-		buttonClockShop.on('click', function (evt) {
-			evt.preventDefault();
-			body.addClass('schedule-opened');
-		});
-	}
-
-	if (buttonClockCinema) {
-		buttonClockCinema.on('click', function (evt) {
-			evt.preventDefault();
-			body.addClass('schedule-opened');
-		});
-	}
-
-	if (buttonClockMenu) {
-		buttonClockMenu.on('click', function (evt) {
-			evt.preventDefault();
-			body.addClass('schedule-opened');
-		});
-	}
-
-	if (buttonClose) {
-		buttonClose.on('click', function (evt) {
-			evt.preventDefault();
-			body.removeClass('schedule-opened');
-		});
-	}
-
-	window.addEventListener('keydown', function (evt) {
-		if (evt.keyCode === ESC) {
-			evt.preventDefault();
-			if (body.hasClass('schedule-opened')) {
-				body.removeClass('schedule-opened');
-			}
-		}
-	});
+$(document).ready(function () {
+  // График работы
+  var body = $('body');
+  var buttonClock = $('.site-navigation__item--clock');
+  var buttonClockFooter = $('.address__item--time');
+  var buttonClockShop = $('.page-shop__link--clock');
+  var buttonClockCinema = $('.shop-info__link--clock');
+  var buttonClockMenu = $('.address-menu__item--clock');
+  var buttonClose = $('.schedule__button');
+  var ESC = 27;
+  if (buttonClock) {
+    buttonClock.on('click', function (evt) {
+      evt.preventDefault();
+      body.addClass('schedule-opened');
+    });
+  }
+  if (buttonClockFooter) {
+    buttonClockFooter.on('click', function (evt) {
+      evt.preventDefault();
+      body.addClass('schedule-opened');
+    });
+  }
+  if (buttonClockShop) {
+    buttonClockShop.on('click', function (evt) {
+      evt.preventDefault();
+      body.addClass('schedule-opened');
+    });
+  }
+  if (buttonClockCinema) {
+    buttonClockCinema.on('click', function (evt) {
+      evt.preventDefault();
+      body.addClass('schedule-opened');
+    });
+  }
+  if (buttonClockMenu) {
+    buttonClockMenu.on('click', function (evt) {
+      evt.preventDefault();
+      body.addClass('schedule-opened');
+    });
+  }
+  if (buttonClose) {
+    buttonClose.on('click', function (evt) {
+      evt.preventDefault();
+      body.removeClass('schedule-opened');
+    });
+  }
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ESC) {
+      evt.preventDefault();
+      if (body.hasClass('schedule-opened')) {
+        body.removeClass('schedule-opened');
+      }
+    }
+  });
 });
