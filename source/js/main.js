c7588e7de038f105d77c81d1ed434220c2303c89
ARTEM LEVANOV <protem@mail.ru>
2020-04-16 10:46:41 +0300

    Корректировка анимации банера

--- source/js/main.js
+++ source/js/main.js
@@ -2,34 +2,23 @@
 
 // Переключение банера
 $(document).ready(function () {
-  var images = ['img/banner-desktop@1x.png', 'https://www.dw.com/image/44504125_303.jpg'];
-  var imagesMobile = ['img/banner-mobile@1x.png', 'https://www.dw.com/image/44504125_303.jpg'];
-  var delay = 7000;
-  var position = 1;
-  var isMobile = $(window).width() < 1024;
-
-  // Смена слайдеров через delay секунд
-  setTimeout(function request() {
-    var image = isMobile ? imagesMobile : images;
-    $('.banner').css('opacity', '0');
-    $('.banner').css('background-image', 'url(' + image[position] + ')');
-    $('.banner').fadeTo(1000, 1);
-
-    position++;
-    if (imagesMobile.length - 1 < position) {
-      position = 0;
-    }
-
-    setTimeout(request, delay);
-
-  }, delay);
-
-  $(window).resize(function () {
-    isMobile = $(window).width() < 1024;
-
-    var image = isMobile ? imagesMobile : images;
-    $('.banner').css('background-image', 'url(' + image[position] + ')');
-  });
+  if ($(window).width() > 1024) {
+    $('.banner').vegas({
+      autoplay: true,
+      slides: [
+        {src: '../../img/banner-desktop@1x.png'},
+        {src: '../../img/uniclo-banner-desktop-img1@1x.png'},
+      ]
+    });
+  } else {
+    $('.banner').vegas({
+      autoplay: true,
+      slides: [
+        {src: '../../img/banner-mobile@1x.png'},
+        {src: '../../img/uniclo-banner-mobile-img1@1x.png'},
+      ]
+    });
+  }
 
   // Открытие/закрытие меню
   var buttonOpenMenu = $('.site-navigation__open-menu');
@@ -309,45 +298,5 @@ $(document).ready(function () {
       $('.store__items--shops.slick-initialized').slick('unslick');
       $('.store__items--services.slick-initialized').slick('unslick');
     }
-  });
-});
-
-// Слайдер для банера на главной
-$(document).ready(function () {
-  var images = ['img/banner-desktop@1x.png', 'https://www.dw.com/image/44504125_303.jpg'];
-  var imagesMobile = ['img/banner-mobile@1x.png', 'https://www.dw.com/image/44504125_303.jpg'];
-  var delay = 5000;
-  var position = 1;
-  var isMobile = $(window).width() < 1024;
-
-  setTimeout(function () {
-    $('.banner').fadeTo(1000, 0.15);
-  }, 4000);
-
-  //Смена слайдеров через delay секунд
-  setTimeout(function request() {
-    var image = isMobile ? imagesMobile : images;
-    $('.banner').css('opacity', '0.15');
-    $('.banner').css('background-image', 'url(' + image[position] + ')');
-    $('.banner').fadeTo(2000, 1);
-
-    position++;
-    if (imagesMobile.length - 1 < position) {
-      position = 0;
-    }
-
-    setTimeout(function () {
-      $('.banner').fadeTo(1000, 0.15);
-    }, 4000);
-
-    setTimeout(request, delay);
-
-  }, delay);
-
-  $(window).resize(function () {
-    isMobile = $(window).width() < 1024;
-
-    var image = isMobile ? imagesMobile : images;
-    $('.banner').css('background-image', 'url(' + image[position] + ')');
   });
 });
